42cfd7bf5fca55bd9b4490f3b097e06e5b9521a3
Tiago Seibel <tiagoseibel@gmail.com>
2019-08-28 21:38:35 -0300

    webaula05

---
+++ webaula05/src/main/java/br/com/empresa/lp1/webaula05/Webaula05Application.java
@@ -0,0 +1,13 @@
+package br.com.empresa.lp1.webaula05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webaula05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webaula05Application.class, args);
+	}
+
+}
---
+++ webaula05/src/test/java/br/com/empresa/lp1/webaula05/Webaula05ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.empresa.lp1.webaula05;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Webaula05ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
